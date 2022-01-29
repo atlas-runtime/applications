cec84be8e73973e1e38a00887284f47ff8645c16
DIMITRIS KARNIKIS <dkarnikis@gmail.com>
2022-01-29 09:15:02 +0200

    Add sample outputs for different inputs for Atlas
    Signed-off-by: DIMITRIS KARNIKIS <dkarnikis@gmail.com>

--- atlas-demo/atlas-client/run.sh
+++ atlas-demo/atlas-client/run.sh
@@ -4,14 +4,13 @@ mkdir $out
 echo "set_allow_charging false" | nc -q 0 127.0.0.1 8423
 for (( c=16; c<=1024 * 1024; c = c * 2 ))
 do                                       
-	rm -f .tmp input
+	rm -f .tmp
 	fallocate -l $c .tmp
-	cat .tmp | sed 's/\x0/a/g' > input
-	../quickjs/src/qjs atlas.js --file benchmarks/crypto_benchmark/demo.js --servers 1 --log $out/remote_$c
-	column -t $out/remote_$c > t ; mv t $out/remote_$c
-	../quickjs/src/qjs atlas.js --file benchmarks/crypto_benchmark/demo.js --log $out/local_$c --local
+	cat .tmp | sed 's/\x0/a/g' > my_input
+	../quickjs/src/qjs atlas.js --file benchmarks/crypto_benchmark/demo.js --log $out/local_$c --local --input my_input
 	column -t $out/local_$c > t ; mv t $out/local_$c
 	echo $c
+	rm -rf my_input
 done                                     
 
 echo "set_allow_charging true" | nc -q 0 127.0.0.1 8423
